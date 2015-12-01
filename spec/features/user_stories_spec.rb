dedfdbf7a9324fcfe18f3e98a39a4e9a5ac1168e
hibreez <4ngus.whitehead@gmail.com>
2015-12-01 10:35:00 +0000

    removed trivial test

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,11 +4,7 @@ describe "User Stories" do
   # In order to use public transport
   # As a customer
   # I want money on my card
-  it 'card has a balance' do
-    expect(card).to respond_to(:balance)
-  end
-
-  it 'card is initialized with a balance of 0' do
+  it 'card has a balance of 0' do
     expect(card.balance).to eq 0
   end
 
